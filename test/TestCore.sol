d883468cf4f12a1dfd0c57c52fc579a4c6ef6002
James Wenzel <jameswenzel@users.noreply.github.com>
2023-05-03 21:27:14 -0700

    add test

---
+++ test/TestCore.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {Test} from "forge-std/Test.sol";
+import {MyContract} from "dep-core/MyContract.sol";
+import {MyStructLib} from "dep-sol/MyStructLib.sol";
+import {MyStruct, MySubStruct} from "dep-core/lib/MyStruct.sol";
+
+contract TestCore is Test {
+    using MyStructLib for MyStruct;
+    MyContract test;
+
+    function setUp() public {
+        test = new MyContract();
+    }
+
+    function testThing() public {
+        MyStruct memory s = MyStruct(0, 0, new MySubStruct[](0));
+        s.withA(1).withB(2);
+        test.run(s);
+    }
+}
